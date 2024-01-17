89188c9b9e0571523106ae9f25c3d92b875876b8
chikoulat <hamidouboum@hotmail.fr>
2024-01-17 23:08:03 +0100

    FEAT: Database seeded. UPDATE: Rows added to product table

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,42 +1,45 @@
 CREATE TABLE skin (
-    id int unsigned primary key auto_increment not null,
+    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
     type VARCHAR(150) NOT NULL
 );
 
 CREATE TABLE user (
-  id int unsigned primary key auto_increment not null,
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
   firstname VARCHAR(255) NOT NULL,
   lastname VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL UNIQUE,
   password VARCHAR(255) NOT NULL,
   age INT NOT NULL,
   city VARCHAR(255) NOT NULL,
-  lat VARCHAR(255),
-  long VARCHAR(255),
+  country VARCHAR(255) NOT NULL,
+  latitutde VARCHAR(255),
+  longitude VARCHAR(255),
   skin_id_1 INT NOT NULL,
   skin_id_2 INT,
   skin_id_3 INT
 );
 
 create table category (
-  id int unsigned primary key auto_increment not null,
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
   name VARCHAR(255) NOT NULL
 );
 create table sub_category (
-  id int unsigned primary key auto_increment not null,
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
   name VARCHAR(255) NOT NULL
 );
 create table product (
-  id int unsigned primary key auto_increment not null,
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
   name VARCHAR(255) NOT NULL,
   description TEXT NOT NULL,
   price FLOAT NOT NULL,
   image VARCHAR(255) NOT NULL,
   product_url VARCHAR(255) NOT NULL,
   category_id INT NOT NULL,
-  sub_cat_1 INT NOT NULL,
-  sub_cat_2 INT,
-  sub_cat_3 INT,
-  skin_type_1 INT NOT NULL,
-  skin_type_2 INT
+  sub_cat_id_1 INT NOT NULL,
+  sub_cat_id_2 INT,
+  sub_cat_id_3 INT,
+  skin_id_1 INT NOT NULL,
+  skin_id_2 INT,
+  skin_id_3 INT
+
 );
