8d72d3634f8a8cea380a42c2bb416609838ee896
chikoulat <hamidouboum@hotmail.fr>
2024-01-17 23:56:13 +0100

    UPDATE: schema tables updated, foreign keys added

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -14,9 +14,9 @@ CREATE TABLE user (
   country VARCHAR(255) NOT NULL,
   latitutde VARCHAR(255),
   longitude VARCHAR(255),
-  skin_id_1 INT NOT NULL,
-  skin_id_2 INT,
-  skin_id_3 INT
+  skin_id_1 INT UNSIGNED NOT NULL,
+  skin_id_2 INT UNSIGNED,
+  skin_id_3 INT UNSIGNED 
 );
 
 create table category (
@@ -34,12 +34,49 @@ create table product (
   price FLOAT NOT NULL,
   image VARCHAR(255) NOT NULL,
   product_url VARCHAR(255) NOT NULL,
-  category_id INT NOT NULL,
-  sub_cat_id_1 INT NOT NULL,
-  sub_cat_id_2 INT,
-  sub_cat_id_3 INT,
-  skin_id_1 INT NOT NULL,
-  skin_id_2 INT,
-  skin_id_3 INT
-
+  category_id INT  UNSIGNED NOT NULL,
+  sub_cat_id_1 INT  UNSIGNED NOT NULL,
+  sub_cat_id_2 INT UNSIGNED,
+  sub_cat_id_3 INT UNSIGNED,
+  skin_id_1 INT  UNSIGNED NOT NULL,
+  skin_id_2 INT UNSIGNED,
+  skin_id_3 INT UNSIGNED 
 );
+
+-- FOREIGN KEYS
+
+-- USER-SKIN
+ALTER TABLE user ADD CONSTRAINT skin_id_1 FOREIGN KEY (`skin_id_1`)
+    REFERENCES skin (id);
+
+ALTER TABLE user ADD CONSTRAINT skin_id_2 FOREIGN KEY (`skin_id_2`)
+    REFERENCES skin (id);
+
+ALTER TABLE user ADD CONSTRAINT skin_id_3 FOREIGN KEY (`skin_id_3`)
+    REFERENCES skin (id);
+
+-- PRODUCT-SKIN
+ALTER TABLE product ADD CONSTRAINT skin_id_1 FOREIGN KEY (`skin_id_1`)
+    REFERENCES skin (id);
+
+ALTER TABLE product ADD CONSTRAINT skin_id_2 FOREIGN KEY (`skin_id_2`)
+    REFERENCES skin (id);
+
+ALTER TABLE product ADD CONSTRAINT skin_id_3 FOREIGN KEY (`skin_id_3`)
+    REFERENCES skin (id);
+
+-- PRODUCT-CATEGORY
+ALTER TABLE product ADD CONSTRAINT product_category FOREIGN KEY (`category_id`)
+    REFERENCES category (id);
+
+-- PRODUCT-SUB CATEGORY
+ALTER TABLE product ADD CONSTRAINT product_sub_category_1 FOREIGN KEY (`sub_cat_id_1`)
+    REFERENCES sub_category (id);
+
+ALTER TABLE product ADD CONSTRAINT product_sub_category_2 FOREIGN KEY (`sub_cat_id_2`)
+    REFERENCES sub_category (id);
+
+ALTER TABLE product ADD CONSTRAINT product_sub_category_3 FOREIGN KEY (`sub_cat_id_3`)
+    REFERENCES sub_category (id);
+
+
