3098e2217b5414068b8c859e04d22546b0ef720c
Kino <faiza.fehri+github@gmail.com>
2024-01-18 07:04:29 +0100

    nav component rectified

--- frontend/src/components/NavigationComponents/Navbar.jsx
+++ frontend/src/components/NavigationComponents/Navbar.jsx
@@ -1,16 +1,48 @@
 import { NavLink } from "react-router-dom";
-import logoWhite from "../../assets/logo-white.png";
+import { useState } from "react";
+import logoblack from "../../assets/logo-black.png";
+import BurgerMenu from "./BurgerMenu";
 
 export default function Navbar() {
-  return (
-    <nav className="text-4xl bg-mineShaft flex flex-row justify-around items-center p-4">
-      <NavLink to="/">
-        <img src={logoWhite} alt="logo" className="h-16" />
-      </NavLink>
+  // remplir ce tableau pour compléter la navbar (le 1er est un exemple)
+  const navlinks = [
+    {
+      id: 1,
+      path: "/",
+      name: "Skincare",
+    },
+    {
+      id: 2,
+      path: "/profile",
+      name: "Profile",
+    },
+    {
+      id: 3,
+      path: "/chatbot",
+      name: "Chatbot",
+    },
+  ];
+  const [menuOpen, setMenuOpen] = useState(false);
 
-      <div className="flex flex-col gap-4">
-        <NavLink to="/user/1"> My Profil</NavLink>
-      </div>
+  return (
+    <nav>
+      <nav className="bg-gradient-to-b from-transparent via-transparent to-slate-400 h-20 flex flex-row justify-center fixed top-0">
+        <nav className="mx-24 flex flex-col justify-center">
+          <img alt="logo" src={logoblack} width={500} />
+        </nav>
+        <div className="md:flex flex-col gap-4 hidden">
+          {navlinks.map((n) => (
+            <NavLink to={n.path} key={n.id}>
+              {n.name}
+            </NavLink>
+          ))}
+        </div>
+        <BurgerMenu
+          navlinks={navlinks}
+          menuOpen={menuOpen}
+          setMenuOpen={setMenuOpen}
+        />
+      </nav>
     </nav>
   );
 }
